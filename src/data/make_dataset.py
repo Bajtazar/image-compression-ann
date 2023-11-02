e6cc8e3470e9e55ed508923239acb25a1d8c952e
Grzegorz Tomasz Czarnecki <01158613@pw.edu.pl>
2023-11-03 00:03:32 +0100

    Linted make_dataset and removed data/external from readme

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -10,7 +10,7 @@ from opendatasets import download
 from utils import get_current_logger
 
 
-FLICKR_DATASET: str = 'https://www.kaggle.com/datasets/hsankesara/flickr-image-dataset'
+FLICKR_DATASET: str = "https://www.kaggle.com/datasets/hsankesara/flickr-image-dataset"
 
 
 def download_flickr_dataset(dataset_path: str) -> None:
@@ -21,9 +21,13 @@ def download_flickr_dataset(dataset_path: str) -> None:
 
 def resolve_raw_dataset(dataset_path: str) -> None:
     if not exists(dataset_path):
-        get_current_logger().info("Flickr dataset has not been detected - would you like to download it automatically or rather do it manually?")
-        command = input('[Press D to download automatically otherwise press any other key]:')
-        if command == 'D':
+        get_current_logger().info(
+            "Flickr dataset has not been detected - would you like to download it automatically or rather do it manually?"
+        )
+        command = input(
+            "[Press D to download automatically otherwise press any other key]:"
+        )
+        if command == "D":
             download_flickr_dataset()
     else:
         get_current_logger().info("Flickr dataset has been found")

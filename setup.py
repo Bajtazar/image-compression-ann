ad3dc21809c45a70ed319112ac994c404f2e24c7
Grzegorz Tomasz Czarnecki <01158613@pw.edu.pl>
2023-11-03 00:02:15 +0100

    Added data/utils.py and started implementing flickr downloader

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name='src',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.0.1',
     description='Bachelor Thesis utilizing an artifical neural networks and discrete wavelet transform in order to achieve state-of-the-art lossy image compression',
     author='Grzegorz Tomasz Czarnecki',
-    license='MIT',
+    license='Zlib',
 )
--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -2,29 +2,41 @@
 import click
 import logging
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+from sys import argv
+from os.path import exists
+
+from opendatasets import download
+
+from utils import get_current_logger
+
+
+FLICKR_DATASET: str = 'https://www.kaggle.com/datasets/hsankesara/flickr-image-dataset'
+
+
+def download_flickr_dataset(dataset_path: str) -> None:
+    get_current_logger().info("Downloading flickr dataset")
+    download(FLICKR_DATASET, dataset_path)
+    get_current_logger().info("Flickr dataset has been successfully downloaded")
+
+
+def resolve_raw_dataset(dataset_path: str) -> None:
+    if not exists(dataset_path):
+        get_current_logger().info("Flickr dataset has not been detected - would you like to download it automatically or rather do it manually?")
+        command = input('[Press D to download automatically otherwise press any other key]:')
+        if command == 'D':
+            download_flickr_dataset()
+    else:
+        get_current_logger().info("Flickr dataset has been found")
 
 
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+@click.argument("input_filepath", type=click.Path())
+@click.argument("output_filepath", type=click.Path())
+def main(input_filepath: str, output_filepath: str) -> None:
+    resolve_raw_dataset(input_filepath)
+    get_current_logger().info("making final data set from raw data")
+    download()
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
+if __name__ == "__main__":
     main()

80f845e198132c01e4454687a364fb19287d4348
Grzegorz Tomasz Czarnecki <01158613@pw.edu.pl>
2023-11-19 14:13:36 +0100

    Added liu4k dataset downloader

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,46 +4,80 @@ import logging
 from pathlib import Path
 from sys import argv
 from os.path import exists
+from typing import Callable
 
-from opendatasets import download
+from opendatasets import download as kaggle_download
+
+from gdown import download_folder as gdrive_download
 
 from utils import get_current_logger
 
 
-FLICKR_DATASET: str = "https://www.kaggle.com/datasets/hsankesara/flickr-image-dataset"
-LIU4K_DATASET: str = (
+FLICKR_DATASET_URL: str = (
+    "https://www.kaggle.com/datasets/hsankesara/flickr-image-dataset"
+)
+LIU4K_DATASET_URL: str = (
     "https://drive.google.com/drive/folders/1FtVQtY2t_ecuy_gzJqZ-CatqrJBAdq_d"
 )
+FLICKR_DATASET_DIR: str = "flickr-image-dataset"
+LIU4K_DATASET_DIR: str = "LIU4K_v2_train"
 
 
 def download_flickr_dataset(dataset_path: str) -> None:
-    get_current_logger().info(f"Downloading {dataset_path} dataset")
-    download(FLICKR_DATASET, dataset_path)
-    get_current_logger().info(
-        f"{dataset_path} dataset has been successfully downloaded"
+    get_current_logger().info("Downloading Flickr dataset")
+    kaggle_download(FLICKR_DATASET_URL, dataset_path)
+    get_current_logger().info("Flickr dataset has been successfully downloaded")
+
+
+def download_liu4k_dataset(dataset_path: str) -> None:
+    get_current_logger().info("Downloading LIU4K dataset")
+    gdrive_download(
+        LIU4K_DATASET_URL, output=dataset_path, quiet=False, use_cookies=False
     )
+    get_current_logger().info("LIU4K dataset has been successfully downloaded")
 
 
-def resolve_raw_dataset(dataset_path: str) -> None:
-    if not exists(dataset_path):
+def resolve_raw_dataset_download(
+    dataset_path: str,
+    dataset_dir: str,
+    dataset_name: str,
+    always_download: bool,
+    download_cb: Callable[[str], None],
+) -> None:
+    if not exists(f"{dataset_path}/{dataset_dir}"):
+        if always_download:
+            return download_cb(dataset_path)
         get_current_logger().info(
-            f"{dataset_path} dataset has not been detected - would you like to download it automatically or rather do it manually?"
+            f"{dataset_name} dataset has not been detected - would you like to download it automatically or rather do it manually?"
         )
         command = input(
             "[Press D to download automatically otherwise press any other key]:"
         )
         if command == "D":
-            download_flickr_dataset()
+            download_cb(dataset_path)
     else:
-        get_current_logger().info(f"{dataset_path} dataset has been found")
+        get_current_logger().info(f"{dataset_name} dataset has been found")
 
 
 @click.command()
-@click.argument("input_filepath", type=click.Path())
-@click.argument("output_filepath", type=click.Path())
-def main(input_filepath: str, output_filepath: str) -> None:
-    resolve_raw_dataset(input_filepath)
-    get_current_logger().info("making final data set from raw data")
+@click.argument("raw_datasets_path", type=click.Path())
+@click.option("--always_download", type=bool, default=False)
+def main(raw_datasets_path: str, always_download: bool) -> None:
+    resolve_raw_dataset_download(
+        raw_datasets_path,
+        FLICKR_DATASET_DIR,
+        "Flickr30k",
+        always_download,
+        download_flickr_dataset,
+    )
+    resolve_raw_dataset_download(
+        raw_datasets_path,
+        LIU4K_DATASET_DIR,
+        "LIU4K",
+        always_download,
+        download_liu4k_dataset,
+    )
+    get_current_logger().info("Done")
 
 
 if __name__ == "__main__":

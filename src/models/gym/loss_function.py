abf45c5137edb0e349b538860f696426a71bd195
Grzegorz Tomasz Czarnecki <grzegorz.czarnecki.2021@gmail.com>
2024-01-05 14:28:39 +0100

    Renamed __cdf to __normal_distribution_cdf

--- src/models/gym/loss_function.py
+++ src/models/gym/loss_function.py
@@ -19,7 +19,7 @@ class NormalDistributionRateDistortionLoss(Module):
         self.__epsilon = float(get_config()["network"]["epsilon"])
 
     @staticmethod
-    def __cdf(mean_: Tensor, standard_deviation: Tensor, points: Tensor) -> Tensor:
+    def __normal_distribution_cdf(mean_: Tensor, standard_deviation: Tensor, points: Tensor) -> Tensor:
         return (1.0 + erf((points - mean_) / ((2.0**0.5) * standard_deviation))) / 2.0
 
     @staticmethod
@@ -31,8 +31,8 @@ class NormalDistributionRateDistortionLoss(Module):
         return -tensor_sum(log2(difference), dim=(1, 2, 3))
 
     def __latent_rate(self, mean_: Tensor, stddev: Tensor, latent: Tensor) -> Tensor:
-        upper_bound = self.__cdf(mean_, stddev, latent + 0.5)
-        lower_bound = self.__cdf(mean_, stddev, latent - 0.5)
+        upper_bound = self.__normal_distribution_cdf(mean_, stddev, latent + 0.5)
+        lower_bound = self.__normal_distribution_cdf(mean_, stddev, latent - 0.5)
         return self.__entropy(upper_bound, lower_bound)
 
     def __hyperlatent_rate(self, hyperlatent: Tensor) -> Tensor:
